5df1fbe61240ca156c6894ea8e92b074331a1854
Owlcuty <taje.chi.i@gmail.com>
2020-12-29 19:15:51 +0300

    lab 3 + t6

--- lb3/t6/main.c
+++ lb3/t6/main.c
@@ -1,4 +1,3 @@
-// Не готово.
 #include <stdio.h>
 #include <math.h>
 
@@ -7,40 +6,38 @@ void main ()
 	int n = 0, x = 0;
 	double W = 0, pW = 0, tmpW = 0;
 	scanf ("%d %d", &x, &n);
+	int last_k = 3;
 	pW = 4.0 / (-2 * pow(x, 3));
 	W = pW;
 	tmpW = pW;
-	int last_k = 3;
-	double pp = 0;
 	
 	for (int k = 4; k <= n; k++)
 	{
-		pW *= -1;
+		pW *= -2;
 		pW /= x;
 		tmpW = pW * (last_k - 5) / (k - 5);
-
-		pp = (1.0 - 27) / (1 - 7);
-		double tmpp = pp;
-		for (int i = 2; i <= k + 7; i++)
-		{
-			tmpp = pp * (pow(i, 3) - 27) / (i - 7);
-			if (tmpp != 0 && finite(tmpp) != 0)
-			{
-				pp = tmpp;
-				tmpW *= pp;
-			}
-		}
-
 		if ((tmpW != 0) && (finite(tmpW) != 0))
 		{
 			pW = tmpW;
 			W += pW;
 			last_k = k;
 		}
-
-
 	}
 
-	printf("%lf\n", W);
+	double P = 1;
+	double pp = 1;
+
+	for (int i = 1; i <= n + 7; i++)
+	{
+		pp = (pow(i, 3) - 27) / (i - 7);
+		if ((pp != 0) && (finite(pp) != 0))
+		{
+			P *= pp;
+		}
+	}
+
+	double res = P * W;
+
+	printf("s = %lf\n", res);
 }
 
